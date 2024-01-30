af37f22cdeb9c6eae69dd4dcce36be5e00b6af9c
eric-yates <eric@lumenalabs.com>
2024-01-29 16:52:13 -0800

    major: extract grapher to new package

--- src/Composite.ts
+++
@@ -1,7 +0,0 @@
-export default class Composite implements SharpComposite {
-	public constructor(_options: CompositeOptions) {}
-}
-
-export interface SharpComposite {}
-
-export interface CompositeOptions {}
