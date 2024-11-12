f91f9c9b528aec3916cfe83a89548c7240ddfd7f
eric-yates <hello@ericthecurious.com>
2024-11-12 11:10:29 -0700

    patch: add spy attributes

--- src/testDoubles/FirBandpassFilter/SpyFirBandpassFilter.ts
+++ src/testDoubles/FirBandpassFilter/SpyFirBandpassFilter.ts
@@ -3,8 +3,17 @@ import FirBandpassFilter, {
 } from '../../components/FirBandpassFilter'
 
 export default class SpyFirBandpassFilter extends FirBandpassFilter {
+    public static callsToConstructor: FirBandpassFilterOptions[] = []
+    public static callsToRun: number[][] = []
+
     public constructor(options: FirBandpassFilterOptions) {
         super(options)
+        this.callsToConstructor.push(options)
+    }
+
+    public run(signal: number[]) {
+        SpyFirBandpassFilter.callsToRun.push(signal)
+        return super.run(signal)
     }
 
     public getUsePadding() {
@@ -13,5 +22,9 @@ export default class SpyFirBandpassFilter extends FirBandpassFilter {
 
     public getUseNormalization() {
         return this.useNormalization
+    }
+
+    public get callsToConstructor() {
+        return SpyFirBandpassFilter.callsToConstructor
     }
 }
